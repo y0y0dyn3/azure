15c8241088973c3a5abbb030050ee341bd1ddddf
y0y0dyn3 <mhumphre@rackspace.com>
2017-06-20 22:15:07 -0500

    Created Azure Auth.  Simplified blobstorage. Used @parameters for cmdlets.

---
+++ authentication/AzureAuth.ps1
@@ -0,0 +1,52 @@
+﻿param(
+
+[parameter (Mandatory=$false)][String]$ProfilePath
+
+)
+
+try {
+
+    $AzureRMSubscription = Get-AzureRmSubscription -ErrorAction Stop
+    Write-Host
+    Write-Host "Already Logged In to $($AzureRMSubscription.SubscriptionName), continuing...." -BackgroundColor DarkGreen
+    Write-Host
+    }
+
+catch {
+
+
+
+if($profilePath){
+    
+    
+    
+    try{
+        $Profile = Login-AzureRmAccount -ErrorAction Stop
+        Save-AzureRmProfile -Profile $Profile -Path $ProfilePath -ErrorAction Stop
+        }
+    catch
+        {
+        
+        Write-Host
+        Write-Host "$($Error[0])" -BackgroundColor Red
+        Write-Host
+
+        }
+    }
+
+else {
+        try{
+            Login-AzureRmAccount -ErrorAction Stop
+            }
+        catch{
+            
+            Write-Host
+            Write-Host "$($Error[0])" -BackgroundColor Red
+            Write-Host
+
+            }
+    
+    }
+
+
+}
--- storage/blobstorage.ps1
+++ storage/blobstorage.ps1
@@ -1,87 +1,45 @@
 ﻿
-
 param(
 [parameter (Mandatory=$true)][guid]$subscriptionID,
 [parameter (Mandatory=$true)][String]$storageAccountName,
 [parameter (Mandatory=$true)][String]$storageAccountKey,
 [parameter (Mandatory=$true)][String]$container,
 [parameter (Mandatory=$true)][String]$blobName, 
-[parameter (Mandatory=$true)][String]$localFile,
-[parameter (Mandatory=$true)][string]$ProfilePath
+[parameter (Mandatory=$true)][String]$localFile
 )
 
 
+$contextparams = @{"StorageAccountName"=$storageAccountName;
+                  "StorageAccountKey"=$storageAccountKey;
+                  "ErrorAction"='Stop';
+                  }
 
-##
-function Set-AzureRMLogin {
- 
-param(
+$blobparams = @{"File"=$localFile;
+               "Container"=$container;
+               "Blob"=$blobName;
+               "Context"=$context;
+               "ErrorAction"='Stop'
+                }
 
-[parameter (Mandatory=$false)][String]$ProfilePath
-
-)
-
-try {
-
-    $AzureRMSubscription = Get-AzureRmSubscription -ErrorAction Stop
-    Write-Host "Already Logged In to $($AzureRMSubscription.SubscriptionName), continuing...."
-    
-    }
-
-catch {
-
-
-
-if($profilePath){
-    
-    $Profile = Login-AzureRmAccount
-    Save-AzureRmProfile -Profile $Profile -Path $ProfilePath   
-    
-    }
-
-else {
-    Login-AzureRmAccount
-    }
-
-
+try{
+$context = New-AzureStorageContext @contextparams 
 }
 
+catch{
+Write-Host
+Write-Host "$($Error[0])" -BackgroundColor Red
+Write-Host
 }
-
-
-Set-AzureRMLogin
-
-$resourceGroupName = Get-AzureRmStorageAccount | where {$_.StorageAccountName -eq $StorageAccountName} #place into try catch with RGN and Name
-
-if($resourceGroupName) #will go away with resource group try/catch.
+    
+if($context)
     {
-     
-    $context = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey # place in try/catch
+        try{
+        Set-AzureStorageBlobContent @blobparams
+        }
     
-    
-    try{
-    Set-AzureStorageBlobContent -File $localFile -Container $container -Blob $blobName -Context $context -ErrorAction Stop
-    }
-    catch{
-    
-    Write-Host "$($Error[0])"
-    
-    }
-
-    }
-
-else{
-
-    Write-Host -ForegroundColor DarkRed "So Sorry!  The Storage Account: $StorageAccountName, may not be valid.  Please verify the account."
-
-    }
-
-
-
-
-
-
-
-
-
-
+        catch{
+        Write-Host
+        Write-Host "$($Error[0])" -BackgroundColor Red
+        Write-Host
+        }
+}
